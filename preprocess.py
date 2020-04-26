86c535bf1858fbd87008bde55855bfc71e9ad11d
Lukas <herin049@umn.edu>
2020-04-26 17:58:21 -0500

    Added preprocessing notebook.

--- preprocess.py
+++ preprocess.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     dataframe = pd.read_csv("data_combined.csv", low_memory=False)
     print(dataframe.columns)
     print(dataframe.head())
+    print(dataframe.describe())
